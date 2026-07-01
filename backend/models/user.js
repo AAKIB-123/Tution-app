baa0b840a99cb25f0b0a0e28e1b6098e2a94920a
Aakib patel <akkupate01@gmail.com>
2026-07-01 11:30:03 +0530

    create user model with password hashing

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,16 +1,49 @@
-import { model } from "mongoose";
+import {Schema, model } from "mongoose";
 import bcrypt from 'bcryptjs';
 
-const User = model("User");
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    mobile: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: "admin",
+        enum: ["admin", "student"],
+    }, 
+    isActive: {
+        type: Boolean,
+        default: true
+    },
 
-model("User", {
-    name: String,
-    email: String + { unique: true },
-    mobile: String,
-    password: String,
-    role: String, default: "admin", roles: ["admin", "student"],
-    isActive: Boolean, default: true
+   
 });
+
+
+UserSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next(); 
+})
+
+export default model("User", UserSchema);
+
 
 
 
